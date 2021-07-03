54822c15c8c612b6d55d9ee11730efc293604e5c
Guillermo Andrada <guillermoandrada@gmail.com>
2021-07-03 20:11:50 -0300

    último commit con favicono y títulos repuestos

---
+++ script.js
@@ -0,0 +1,8 @@
+function age(dateString) {
+            let birth = new Date(dateString);
+            let now = new Date();
+            let beforeBirth = ((() => { birth.setDate(now.getDate()); birth.setMonth(now.getMonth()); return birth.getTime() })() < birth.getTime()) ? 0 : 1;
+            edadActual = now.getFullYear() - birth.getFullYear() - beforeBirth;
+        }
+        age('12/13/1986');
+        document.getElementById("edad").innerHTML = edadActual;
